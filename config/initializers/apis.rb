7716007d5be5eb0d23f16f7b3e3fb5e1a27e5d1f
Amber Smith <smithambera@gmail.com>
2016-08-17 10:03:25 -0400

    added search bar

--- config/initializers/apis.rb
+++ config/initializers/apis.rb
@@ -1,7 +1,7 @@
 # to use gracenote API
 spec = {:clientID => ENV["Client_ID"], :clientTag => ENV["Client_Tag"]}
 GNAPI = Gracenote.new(spec)
-GNAPI.registerUser
+# GNAPI.registerUser
 
 # to use the movie database
 Tmdb::Api.key(ENV["tmdb"])
