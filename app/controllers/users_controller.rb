38281fb2dd1c59149a346db339cb913c1d324aa9
dwegrzen <dwegrzen@gmail.com>
2016-08-22 12:25:21 -0400

    new routes for album controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -74,8 +74,8 @@ before_action :userinfo
       if current_user
         @userqueuedshows = current_user.shows.pluck(:gn_id)
         @userqueuedmusic = current_user.albums.pluck(:gn_id)
-        finishedshows = current_user.stackings.where(finished: true)&.where(:media_type "Show").pluck(:media_id)
-        finishedmusic = current_user.stackings.where(finished: true)&.where(:media_type "Album").pluck(:media_id)
+        finishedshows = current_user.stackings.where(finished: true)&.where(media_type: "Show").pluck(:media_id)
+        finishedmusic = current_user.stackings.where(finished: true)&.where(media_type: "Album").pluck(:media_id)
         @userfinishedshows = current_user.shows.find(finishedshows).pluck(:gn_id)
         @userfinishedmusic = current_user.albums.find(finishedmusic).pluck(:gn_id)
 
