9d0a6fff9159c5f5d80421b389bcc2c80c98c2cd
dwegrzen <dwegrzen@gmail.com>
2016-08-30 12:35:22 -0400

    changed a migration

--- db/migrate/20160830132446_add_password_reset_boolean.rb
+++ db/migrate/20160830132446_add_password_reset_boolean.rb
@@ -1,6 +1,5 @@
 class AddPasswordResetBoolean < ActiveRecord::Migration[5.0]
   def change
-    add_column :users, :password_reset_token, :string
     add_column :users, :password_reset_status, :boolean
   end
 end
